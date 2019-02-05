ba6584df78c2c7e30c9c55f1dff23a6546881949
Michael Tobia <michaeltobia2019@u.northwestern.edu>
2019-02-04 20:17:54 -0600

    problem 2 fix and clean

--- problem_2.py
+++ problem_2.py
@@ -1,9 +1,9 @@
 i = 1
 j = 2
-j_old = 2
+
 sum = 0
-while j<4*(10**6):
-    print i
+while i<4*(10**6):
+    print i, j
     j_old = j
     j += i
     if not bool(i%2):
@@ -11,4 +11,4 @@ while j<4*(10**6):
     i = j_old
 
 
-# sum = 1089154
+# sum = 4613732
