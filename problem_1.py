94f2e20719f07b22f737de696e7d22fffe2cb838
Michael Tobia <michaeltobia2019@u.northwestern.edu>
2019-02-04 18:05:04 -0600

    problem 1 attempt 2

--- problem_1.py
+++ problem_1.py
@@ -2,9 +2,9 @@ sum = 0
 i = 0
 
 while i<1000:
-    if (not bool(i%5)) != (not bool(i%3)):
+    if (not bool(i%5)) or (not bool(i%3)):
         sum += i
+        print i
     i += 1
-    print i
 
-# sum = 20003
+# sum = 233168
